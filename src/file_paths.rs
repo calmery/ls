e0dc793ec0644c182eca6d2f1c17dc2945639456
Marei Kikukawa <contact@calmery.me>
2019-10-28 02:41:42 +0900

    ファイルの一覧を相対パスで表示する

--- src/file_paths.rs
+++ src/file_paths.rs
@@ -4,11 +4,20 @@ use std::path::Path;
 pub fn get(path: &Path) -> std::io::Result<Vec<String>> {
     let mut file_paths: Vec<String> = Vec::new();
 
+    // Reference: https://doc.rust-lang.org/std/string/struct.String.html#method.as_str
+    let current_directory_path = format!("{}{}", path.display().to_string(), "/");
+    let current_directory = current_directory_path.as_str();
+
     // Reference: https://doc.rust-lang.org/std/fs/fn.read_dir.html
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let current_file_path = entry.path();
-        file_paths.push(current_file_path.display().to_string());
+        file_paths.push(
+            current_file_path
+                .display()
+                .to_string()
+                .replace(current_directory, ""),
+        );
     }
 
     Ok(file_paths)
