3932087a7ac5ed748b54d7d98a6c184518a2728d
Marei Kikukawa <contact@calmery.me>
2019-10-28 01:31:37 +0900

    現在のディレクトリにあるファイルの一覧を表示する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    // Reference: https://doc.rust-lang.org/std/env/fn.current_dir.html
+    let current_directory = env::current_dir()?;
+    println!("{}", current_directory.display());
+
+    // Reference: https://doc.rust-lang.org/std/path/struct.PathBuf.html#examples-3
+    let current_directory_path = current_directory.as_path();
+
+    // Reference: https://doc.rust-lang.org/std/fs/fn.read_dir.html
+    for entry in fs::read_dir(current_directory_path)? {
+        let entry = entry?;
+        let current_file_path = entry.path();
+        println!("{}", current_file_path.display());
+    }
+
+    Ok(())
 }
