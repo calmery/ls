87f048bedf6c0884d080beb333d2ecbe03bcf649
Marei Kikukawa <contact@calmery.me>
2019-10-28 01:48:17 +0900

    ファイルの一覧を返す関数を作り、main 関数で結果を表示する

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-fn ls(path: &Path) -> std::io::Result<()> {
+fn get_file_paths(path: &Path) -> std::io::Result<Vec<String>> {
+    let mut file_paths: Vec<String> = Vec::new();
+
     // Reference: https://doc.rust-lang.org/std/fs/fn.read_dir.html
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let current_file_path = entry.path();
-        println!("{}", current_file_path.display());
+        file_paths.push(current_file_path.display().to_string());
     }
 
-    Ok(())
+    Ok(file_paths)
 }
 
 fn main() -> std::io::Result<()> {
@@ -20,8 +22,11 @@ fn main() -> std::io::Result<()> {
 
     // Reference: https://doc.rust-lang.org/std/path/struct.PathBuf.html#examples-3
     let current_directory_path = current_directory.as_path();
+    let file_paths = get_file_paths(current_directory_path)?;
 
-    ls(current_directory_path)?;
+    for file_path in file_paths {
+        println!("{}", file_path);
+    }
 
     Ok(())
 }
