b519303f1589f80263c7ed95859ca64aad19cb2d
Marei Kikukawa <contact@calmery.me>
2019-10-28 01:40:25 +0900

    関数を切り分ける

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::env;
 use std::fs;
+use std::path::Path;
+
+fn ls(path: &Path) -> std::io::Result<()> {
+    // Reference: https://doc.rust-lang.org/std/fs/fn.read_dir.html
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let current_file_path = entry.path();
+        println!("{}", current_file_path.display());
+    }
+
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     // Reference: https://doc.rust-lang.org/std/env/fn.current_dir.html
@@ -9,12 +21,7 @@ fn main() -> std::io::Result<()> {
     // Reference: https://doc.rust-lang.org/std/path/struct.PathBuf.html#examples-3
     let current_directory_path = current_directory.as_path();
 
-    // Reference: https://doc.rust-lang.org/std/fs/fn.read_dir.html
-    for entry in fs::read_dir(current_directory_path)? {
-        let entry = entry?;
-        let current_file_path = entry.path();
-        println!("{}", current_file_path.display());
-    }
+    ls(current_directory_path)?;
 
     Ok(())
 }
